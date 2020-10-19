909f84d3d6d4c3ef75cc7169722f59d8f5230517
Charlotte <c.libak@hotmail.com>
2020-10-19 08:07:30 +0200

    quiz

--- quiz2/js/js.js
+++ quiz2/js/js.js
@@ -1,23 +1,22 @@
 var pos = 0
 var test;
-var test_status;
 var question;
 var choice;
 var choices;
 var chA;
-var chaB;
+var chB;
 var chC;
 var correct = 0;
 
 var questions = [
-	["Hvad hedder jeg","charlotte","cecilie","camilla","A"],
-	["Hvad hedder jeg","charlotte","cecilie","camilla","A"],
-	["Hvad hedder jeg","charlotte","cecilie","camilla","A"],
-	["Hvad hedder jeg","charlotte","cecilie","camilla","A"]
-
+	["Inside which HTML element do we put the JavaScript?","&#60script&#62","&#60javascript&#62","&#60js&#62","A"],
+	["What is the correct syntax for referring to an external script called 'xxx.js'?","&#60script name='xxx.js'&#62","&#60script src='xxx.js'&#62","	&#60script href='xxx.js'&#62","B"],
+	["How do you write 'Hello World' in an alert box?","window('Hello World')","alertBox('Hello World')","alert('Hello World')","C"],
+	["How do you call a function named 'myFunction'?","call myFunction()","myFunction()","call function myFunction()","B"],
+	["How does a FOR loop start?","for(var i = 0; i<5; i++)","for(var i = 0; i<5;)","for(i<+5; i++)","A"]
 ];
 
-export function printQuestion (){
+ function printQuestion (){
 	test = document.getElementById("test");
 	if(pos >= questions.length){
 		test.innerHTML = "<h2> You got "+correct+" of "+questions.length+" questions correct</h2><button id='btn2'>Restart</button>";
@@ -38,7 +37,6 @@ export function printQuestion (){
 	test.innerHTML += "<input type='radio' name='choices' value='C'>"+chC+"<br><br>";
 	test.innerHTML += "<button id='btn'>Submit</button>";
 	document.getElementById("btn").addEventListener("click", check);
-	console.log("hej");
 }
 
 function check(){
